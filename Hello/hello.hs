e2300399c40f02f075623444f534cd35f11549d1
Alexey Gaziev <alex.gaziev@gmail.com>
2014-10-31 12:58:05 +0800

    First steps 3

--- Hello/hello.hs
+++ Hello/hello.hs
@@ -3,16 +3,6 @@ import System.Environment
 
 main :: IO ()
 main = do
-    args <- getArgs
-    putStrLn $ "Result is " ++ (show $ parseArgs args)
-
-parseArgs :: [String] -> Integer
-parseArgs (a : op : b : [])
-  | op == "+" = num1 + num2
-  | op == "*" = num1 * num2
-  | op == "-" = num1 - num2
-  | op == "/" = num1 `div` num2
-  | otherwise = 0
-    where num1 = read a :: Integer
-          num2 = read b :: Integer
-parseArgs _ = 0
+    putStrLn "Please enter your name:"
+    line <- getLine
+    putStrLn $ "Hello " ++ line
