01ac3fc74f5c054941b855045ecf2a39a45010eb
Alexey Gaziev <alex.gaziev@gmail.com>
2014-10-31 13:28:46 +0800

    Parsing

---
+++ SimpleParser/Main.hs
@@ -0,0 +1,51 @@
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+import Control.Monad
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+                   Left err  -> "No match: " ++ show err
+                   Right val -> "Found value"
+
+parseString :: Parser LispVal
+parseString = do
+                char '"'
+                x <- many (noneOf "\"")
+                char '"'
+                return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+              first <- letter <|> symbol
+              rest <- many (letter <|> digit <|> symbol)
+              let atom = first:rest
+              return $ case atom of
+                         "#t" -> Bool True
+                         "#f" -> Bool False
+                         _    -> Atom atom
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+main :: IO ()
+main = do
+    args <- getArgs
+    putStrLn $ readExpr (args !! 0)
