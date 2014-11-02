bc346100f74ab10a9cc411948b0981f394cdeb70
Alexey Gaziev <alex.gaziev@gmail.com>
2014-11-02 11:16:29 +0800

    Simlpe compiling and checking Main.hs with bash

---
+++ SimpleParser/check.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ ! -f ./parser ];
+then
+  ghc -o parser --make Main.hs
+else
+  continue
+fi
+
+./parser 1
+./parser 25
+./parser "12.4312341234"
+./parser "12.43L1"
+./parser "#d1234"
+./parser "#xFA12"
+./parser "#b1101"
+./parser "#o176"
+./parser "3/12"
+./parser "4+18i"
+./parser "%atom"
+./parser "#t"
+./parser "\"string\""
+./parser "#\\s"
+./parser "(1 2 3)"
+./parser "(1 2 . 4)"
+./parser "'12"
+./parser "\`12"
+./parser ",12"
+./parser "#(1 (1 2 . 3) \"string\" #\\s)"
+
+rm ./parser
