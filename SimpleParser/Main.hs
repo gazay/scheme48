196fce4ac7b3121b98f8d83186a601bafccd8a72
Alexey Gaziev <alex.gaziev@gmail.com>
2014-11-08 23:22:30 +0800

    Show error WIP

--- SimpleParser/Main.hs
+++ SimpleParser/Main.hs
@@ -59,6 +59,24 @@ showVal (Vector contents) = "(" ++ show contents ++ ")"
 
 instance Show LispVal where show = showVal
 
+showError :: LispError -> String
+showError (UnboundVar message varname)  = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func)    = message ++ ": " ++ show func
+showError (NumArgs expected found)      = "Expected " ++ show expected
+                                       ++ " args; found values " ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                       ++ ", found " ++ show found
+showError (Parser parseErr)             = "Parse error at " ++ show parseErr
+
+instance Show LispError where show = showError
+
+instance Error LispError where
+    noMsg = Default "An error has occurred"
+    strMsg = Default
+
+type ThrowsError = Either LispError
+
 parseString :: Parser LispVal
 parseString = do
                 char '"'
