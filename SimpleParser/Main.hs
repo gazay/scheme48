f04605451e966bdc13ce296beba2a8d54d90d6c0
Alexey Gaziev <alex.gaziev@gmail.com>
2014-11-13 19:40:46 +0800

    If matching

--- SimpleParser/Main.hs
+++ SimpleParser/Main.hs
@@ -245,6 +245,11 @@ eval val@(Number _) = return val
 eval val@(Atom _) = return val
 eval val@(String _) = return val
 eval (List [Atom "quote", val]) = return val
+eval (List [Atom "if", pred, conseq, alt]) =
+    do result <- eval pred
+       case result of
+         Bool False -> eval alt
+         otherwise  -> eval conseq
 eval (List (Atom func : args)) = mapM eval args >>= apply func
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
